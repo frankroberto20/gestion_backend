a5acad806c12ba6855158aef25f6734fbbdf8f56
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2021-01-14 17:53:41 -0400

    Allowed methods in cors

--- gestion_backend/settings.py
+++ gestion_backend/settings.py
@@ -30,6 +30,14 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 # Application definition
 
