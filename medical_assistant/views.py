f265c309af449318dc4c0a7f88edbd62cdb85852
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2021-01-13 23:44:43 -0400

    Fixed search patients view

--- medical_assistant/views.py
+++ medical_assistant/views.py
@@ -496,10 +496,10 @@ def search_patients(request):
     if request.method == 'POST':
         search_str = json.loads(request.body).get('searchText')
 
-        patient = Usuario.objects.filter(
-            username__icontains=search_str) | Usuario.objects.filter(
-            Cedula__icontains=search_str) | Usuario.objects.filter(
-            tipoUsuario=3) 
+        patients = Usuario.objects.filter(
+            username__startswith=search_str) | Usuario.objects.filter(
+            Cedula__startswith=search_str) | Usuario.objects.filter(
+            first_name__startswith=search_str) & Usuario.objects.filter(
+            tipoUsuario=3)
         
-        data = patient.values()
-        return JsonResponse(list(data), safe=False)
+        return JsonResponse([patient.serialize() for patient in patients], safe=False)
